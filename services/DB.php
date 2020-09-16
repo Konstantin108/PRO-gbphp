7381a73e1c413278b92e50ccab4033a961c56166
Kostya <Kpstya@mail.ru>
2020-09-16 22:06:46 +0300

    added namespaces and new autoloader

--- services/DB.php
+++ services/DB.php
@@ -3,49 +3,10 @@ namespace app\services;
 
 class DB
 {
-    private $config = [
-        'driver' => 'mysql',
-        'host' => '127.0.0.1',
-        'db' => 'gbphp',
-        'charset' => 'UTF8',
-        'login' => 'root',
-        'password' => 'root'
-    ];
 
-    private $connection;
-
-    public function getConnection()
+    public function find($sql)
     {
-        if (empty($this->connection)){
-            $this->connection = new \PDO(
-            $this->getSdn(),
-            $this->config['login'],
-            $this->config['password']
-            );
-            $this->connection->setAttribute(
-                \PDO::ATTR_DEFAULT_FETCH_MODE,
-                \PDO::FETCH_ASSOC
-            );
-        }
-        return $this->connection;
-    }
-
-    private function getSdn()
-    {
-        return sprintf (
-            "%s:host=%s;dbname=%s;charset=%s",
-            $this->config['driver'],
-            $this->config['host'],
-            $this->config['db'],
-            $this->config['charset']
-        );
-    }
-
-    public function find($sql, $params)
-    {
-        $PDOStatement = $this->getConnection()->prepare($sql);
-        $PDOStatement->execute($params);
-        return $PDOStatement->fetch();
+        return 'find' . $sql;
     }
 
     public function findAll($sql)
